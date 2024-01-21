160e6fc55282ec6a1ff2ef7d72402be656232a9b
A7med ismail <111844236+Ahmeddismail@users.noreply.github.com>
2024-01-21 04:13:07 +0200

    Add files via upload

---
+++ include/BookList.h
@@ -0,0 +1,28 @@
+#ifndef BOOKLIST_H
+#define BOOKLIST_H
+#include <iostream>
+#include<string>
+using namespace std;
+#include"Book.h"
+#include"stdlib.h"
+class BookList
+{
+private:
+	Book* books;
+	int capacity;
+	int BookCount;
+public:
+	BookList();
+	BookList(int c);
+	~BookList();
+	void addBook(const Book&);
+	Book* SearchBook(string name);
+	Book* SearchBook(int id);
+	void deleteBook(int id);
+	friend ostream& operator<<(ostream&, const BookList&);
+	Book get_the_highest_rated_book();
+	void get_box_for_user(const User&);
+	Book& operator[](int position);
+};
+
+#endif // BOOKLIST_H
